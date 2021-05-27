52488edd7bd9bc43668cd0a58a97f046c1e09051
Donn Eddy <maindric@live.com>
2021-05-28 04:40:59 +0930

    Added src and fixed image name

---
+++ src/source.js
@@ -0,0 +1,16 @@
+class Movie
+{
+    constructor(name, image, releaseDate, description, genres, actors)
+    {
+        this.name = name;
+        this.image = image;
+        this.releaseDate = releaseDate;
+        this.description = description;
+        this.genres = genres;
+        this.actors = actors;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    //Stuff
+})
